f4b084b3a07708b84391d1f17f03e13c1061a478
Austin Baxley <ausbaxley@gmail.com>
2021-08-20 17:11:50 -0400

    updated background

--- src/App.js
+++ src/App.js
@@ -1,32 +1,42 @@
+import React, { Component } from "react";
 import "./App.css";
 import { Layout, Header, Navigation, Drawer, Content } from "react-mdl";
+import Main from "./components/main";
+import { Link } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="demo-big-content">
-      <Layout>
-        <Header title="Title" scroll>
-          <Navigation>
-            <a href="/">Link</a>
-            <a href="/">Link</a>
-            <a href="/">Link</a>
-            <a href="/">Link</a>
-          </Navigation>
-        </Header>
-        <Drawer title="Title">
-          <Navigation>
-            <a href="/">Link</a>
-            <a href="/">Link</a>
-            <a href="/">Link</a>
-            <a href="/">Link</a>
-          </Navigation>
-        </Drawer>
-        <Content>
-          <div className="page-content" />
-        </Content>
-      </Layout>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="demo-big-content">
+        <Layout
+          style={{
+            background: "src=(./Assets/atl_night.jpg) center / cover",
+          }}
+        >
+          <Header title="Austin Baxley" scroll>
+            <Navigation>
+              <Link to="/aboutme">About Me</Link>
+              <Link to="/projects">Projects</Link>
+              <Link to="/contact">Contact</Link>
+              <Link to="/resume">Resume</Link>
+            </Navigation>
+          </Header>
+          <Drawer title="Austin Baxley">
+            <Navigation>
+              <Link to="/aboutme">About Me</Link>
+              <Link to="/projects">Projects</Link>
+              <Link to="/contact">Contact</Link>
+              <Link to="/resume">Resume</Link>
+            </Navigation>
+          </Drawer>
+          <Content>
+            <div className="page-content" />
+            <Main />
+          </Content>
+        </Layout>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/projects.js
+++ src/components/projects.js
@@ -1,10 +1,38 @@
 import React, { Component } from "react";
+import { Card } from "react-mdl";
+import { CardTitle } from "react-mdl";
+import { CardText } from "react-mdl";
+import { CardActions } from "react-mdl";
+import { Button } from "react-mdl";
+import { CardMenu } from "react-mdl";
+import { IconButton } from "react-mdl";
 
 class Projects extends Component {
   render() {
     return (
       <div>
-        <h1>Projects Page</h1>
+        <Card shadow={0} style={{ width: "512px", margin: "auto" }}>
+          <CardTitle
+            style={{
+              color: "#fff",
+              height: "176px",
+              background:
+                "url(http://www.getmdl.io/assets/demos/welcome_card.jpg) center / cover",
+            }}
+          >
+            Welcome
+          </CardTitle>
+          <CardText>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
+            sagittis pellentesque lacus eleifend lacinia...
+          </CardText>
+          <CardActions border>
+            <Button colored>Get Started</Button>
+          </CardActions>
+          <CardMenu style={{ color: "#fff" }}>
+            <IconButton name="share" />
+          </CardMenu>
+        </Card>
       </div>
     );
   }
